e01c05460e5844738bb093695da53afba4fae82d
Adrian Dowst <aadowst@gmail.com>
2022-07-04 19:40:28 -0700

    done

---
+++ Fundamentals/Python_Fundamentals/functions_basic_ii.py
@@ -0,0 +1,34 @@
+# def countdown(input):
+#     for x in range(input, -1, -1):
+#         print(x)
+# countdown(8)
+
+# def print_and_return(a, b):
+#     print(a)
+#     return b
+# print_and_return(2,3)
+
+# def first_plus_length(list):
+#     return list[0] + len(list)
+# test = first_plus_length([1,2,3,4,5])
+# print(test)
+
+# def greater_than_second(list):
+#     new_list = []
+#     for i in range(len(list)):
+#         if list[i] > list[1]:
+#             new_list.append(list[i])
+#     if len(new_list) < 2:
+#         return False
+#     else:
+#         return new_list
+# test = greater_than_second([2,3,1,5,3,0])
+# print(test)
+
+# def this_length_that_value(size, value):
+#     new_list = []
+#     for x in range(size):
+#         new_list.append(value)
+#     return new_list
+# test = this_length_that_value(4,7)
+# print(test)
