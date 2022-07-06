7f40d2ef727240b907a19d1136a373c020cd0cce
Adrian Dowst <aadowst@gmail.com>
2022-07-05 21:32:12 -0700

    done

---
+++ Fundamentals/Python_Fundamentals/Float_vs_Int.py
@@ -0,0 +1,9 @@
+some_float = 3.5
+some_int = 4
+
+sum = int(some_float) + some_int
+print(type(sum), sum)
+
+import random
+rand_num = random.randint(2,5)
+print(rand_num)
---
+++ Fundamentals/Python_Fundamentals/testing_python.py
@@ -0,0 +1,25 @@
+# import random
+
+# print('Welcome to Python!')
+
+# print('This is a loop printing 5 times')
+# for x in range(1, 6):
+#     print(f'x is: {x}')
+
+# weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+# day = random.choice(weekdays)
+
+# print(f'Today is: {day}')
+
+# if day == 'Monday':
+#     print('It will be a long week!')
+# elif(day == 'Friday'):
+#     print('Woohoo, time for the weekend!')
+# else:
+#     print('Not quite there yet.')
+
+my_list = ["breakfast", "lunch", "dinner"]
+meal_order = enumerate(my_list)
+print(list(meal_order))
+print(type(meal_order))
+print(meal_order[1])
