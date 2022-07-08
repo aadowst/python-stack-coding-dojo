d0f1691347fbb99013f34b2f9e26789ef5d5cc7a
Adrian Dowst <aadowst@gmail.com>
2022-07-07 19:08:12 -0700

    initial commit

---
+++ Flask/Fundamentals/hello_flask/understanding_routing/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+	return 'Understanding Routing'
+
+@app.route('/dojo')
+def hello_dojo():
+	return 'Dojo!'
+
+@app.route('/say/<string:repeat_me>')
+def repeat_me(repeat_me):
+	return 'Hi, ' + repeat_me
+
+@app.route('/<int:how_many>/<string:phrase>')
+def said_it(how_many, phrase):
+
+	said_it = phrase*how_many
+	return f"You wanted me to say {said_it}"
+
+if __name__=="__main__":
+	app.run(debug=True)
