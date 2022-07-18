866b8b768cf1ccd3177098518c9e5436ed9b8200
Adrian Dowst <aadowst@gmail.com>
2022-07-18 07:58:55 -0700

    not needed

--- MySQL_and_Flask/file_structure_template/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-from flask import Flask
-app = Flask(__name__)
-
-app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
--- MySQL_and_Flask/file_structure_template/flask_app/controllers/controller_name.py
+++
@@ -1,3 +0,0 @@
-from flask import render_template, redirect, request, session
-
-from flask_app.model.model_name import Name
